7134ee06c87860a3154e0bb5673ec912a07c5d12
Carlos Andres Lopez <asrael@utp.edu.co>
2016-05-16 11:34:33 -0500

    Update dyd.py

--- dyd.py
+++ dyd.py
@@ -1,29 +1,33 @@
 import os,sys
 import pygame 
 
+ROJO=(255,0,0)
+
 class Cuadro:
         def __init__(self,rect):
             self.rect = pygame.Rect(rect)
             self.click = False
             self.image = pygame.Surface(self.rect.size).convert()
-            self.image.fill((255,0,0))
+            self.image.fill(ROJO)
+
         def update(self,surface):
             if self.click:
                 self.rect.center = pygame.mouse.get_pos()
             surface.blit(self.image,self.rect)
 
-def main(elemento,jugador):
-        Juego(jugador)
-        elemento.fill(0)
-        jugador.update(elemento)
+def main(pantalla,cuadro):
+        #Captura de teclas
+        Juego(cuadro)
+        pantalla.fill(0)
+        cuadro.update(pantalla)
 
-def Juego(Player):
+def Juego(cuadro):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if Player.rect.collidepoint(event.pos):
-                    Player.click = True
+                if cuadro.rect.collidepoint(event.pos):
+                    cuadro.click = True
             elif event.type == pygame.MOUSEBUTTONUP:
-                Player.click = False
+                cuadro.click = False
             elif event.type == pygame.QUIT:
                 pygame.quit(); sys.exit()
 
@@ -38,4 +42,3 @@ if __name__ == "__main__":
             main(pantalla,cuadro)
             pygame.display.update()
             reloj.tick(60)
-
